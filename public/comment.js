04b092516ccf4174c09496315ebc81607c6795c9
junamee <skawn11114@gmail.com>
2021-03-19 03:10:49 +0900

    Add: 코멘트업데이트/북마크 전환.

--- public/comment.js
+++ public/comment.js
@@ -5,19 +5,38 @@ import {
    _
 } from "./utill.js";
 
+const switchBookMark = (wrappingBox) => {
+   const bookMark = wrappingBox.children[0];
+   const newBookMark = _.create('div');
+   newBookMark.innerHTML =
+      `<svg aria-label="삭제" class="_8-yf5 " fill="#262626" height="40" viewBox="0 0 48 48" width="40">
+      <path
+         d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 28.9 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1z">
+      </path>
+      </svg>
+      `
+   wrappingBox.replaceChild(newBookMark, bookMark)
+}
+
 const commentSave = (inputMode) => {
+
    const saveBtn = inputMode.children[1];
    const showMode = inputMode.parentNode.children[1];
 
    saveBtn.addEventListener('click', ({
       target
    }) => {
+      const wrappingBox = target.closest('.card_tpl');
+      switchBookMark(wrappingBox);
+
+      const targetUrl = wrappingBox.children[1].href;
       const comment = target.parentNode.children[0].value;
       showMode.innerText = comment;
       showMode.classList.remove('under');
       inputMode.classList.add('hide');
 
       const obj = {
+         'targetUrl': targetUrl,
          'save': true,
          'comment': comment
       }
@@ -25,6 +44,7 @@ const commentSave = (inputMode) => {
       sendAjax(url, obj, null);
    })
 }
+
 
 const switchMode = (target) => {
    const parents = target.parentNode;
